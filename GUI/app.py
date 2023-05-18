9560798011d758b6593b6bcebcaab667d0a3b6bc
Yousef AlZayat <67283820+YousefAlzayat@users.noreply.github.com>
2023-05-18 22:25:55 +0300

    Updated looks of main app

--- GUI/app.py
+++ GUI/app.py
@@ -3,6 +3,7 @@ import customtkinter
 # from admin import AdminWindow
 from login import LogInWindow
 from register import RegisterWindow
+from view import ViewWindow
 
 class mainApp(customtkinter.CTk):
     def __init__(self):
@@ -18,13 +19,22 @@ class mainApp(customtkinter.CTk):
         label.place(relx=0.5, rely=0.125, anchor=tkinter.CENTER)
 
         viewButton = customtkinter.CTkButton(master=buttonFrame, text="View", command=self.view, height=40, width=100)
-        viewButton.place(relx=0.875, rely=0.325, anchor=tkinter.CENTER)
+        viewButton.place(relx=0.75, rely=0.325, anchor=tkinter.CENTER)
+
+        viewLabel = customtkinter.CTkLabel(master=buttonFrame, text="View Available Trips", font=customtkinter.CTkFont(size=20, weight="bold"))
+        viewLabel.place(relx=0.375, rely=0.325, anchor=tkinter.CENTER)
 
         signInButton = customtkinter.CTkButton(master=buttonFrame, text="Sign In", command=self.signIn, height=40, width=100)
-        signInButton.place(relx=0.875, rely=0.5, anchor=tkinter.CENTER)
+        signInButton.place(relx=0.75, rely=0.5, anchor=tkinter.CENTER)
+
+        logInLabel = customtkinter.CTkLabel(master=buttonFrame, text="Sign into Your Account", font=customtkinter.CTkFont(size=20, weight="bold"))
+        logInLabel.place(relx=0.375, rely=0.5, anchor=tkinter.CENTER)
 
         registerButton = customtkinter.CTkButton(master=buttonFrame, text="Register", command=self.register, height=40, width=100)
-        registerButton.place(relx=0.875, rely=0.675, anchor=tkinter.CENTER)
+        registerButton.place(relx=0.75, rely=0.675, anchor=tkinter.CENTER)
+
+        registerLabel = customtkinter.CTkLabel(master=buttonFrame, text="Register a New Account", font=customtkinter.CTkFont(size=20, weight="bold"))
+        registerLabel.place(relx=0.375, rely=0.675, anchor=tkinter.CENTER)
 
         exitButton = customtkinter.CTkButton(master=buttonFrame, text="Exit", command=self.exit, height=40, width=80)
         exitButton.place(relx=0.1, rely=0.9, anchor=tkinter.CENTER)
@@ -33,7 +43,9 @@ class mainApp(customtkinter.CTk):
         self.destroy()
     
     def view(self):
-        print("view")
+        self.destroy()
+        view = ViewWindow()
+        view.mainloop()
 
     def signIn(self):
         self.destroy()
