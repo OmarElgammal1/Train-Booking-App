0f4f6e8cbe7efca5a2a758a8cc31c43150e80c73
Yousef AlZayat <67283820+YousefAlzayat@users.noreply.github.com>
2023-05-18 18:34:14 +0300

    Added register as Admin

--- register.py
+++ register.py
@@ -15,26 +15,37 @@ class RegisterWindow(customtkinter.CTk):
         self.label = customtkinter.CTkLabel(master=self.formFrame, text="Register", font=customtkinter.CTkFont(size=30, weight="bold"))
         self.label.place(relx=0.5, rely=0.125, anchor=tkinter.CENTER)
 
-        self.eMailEntry = customtkinter.CTkEntry(master=self.formFrame, placeholder_text="E-Mail", height=40, width=225)
-        self.eMailEntry.place(relx=0.25, rely=0.5, anchor=tkinter.CENTER)
-
         self.nameEntry = customtkinter.CTkEntry(master=self.formFrame, placeholder_text="Name", height=40, width=465)
         self.nameEntry.place(relx=0.5, rely=0.325, anchor=tkinter.CENTER)
 
-        self.passwordReEntry = customtkinter.CTkEntry(master=self.formFrame, show="*", placeholder_text="Enter Password", height=40, width=225)
-        self.passwordReEntry.place(relx=0.25, rely=0.675, anchor=tkinter.CENTER)
+        self.eMailEntry = customtkinter.CTkEntry(master=self.formFrame, placeholder_text="E-Mail", height=40, width=200)
+        self.eMailEntry.place(relx=0.225, rely=0.5, anchor=tkinter.CENTER)
 
-        self.passwordEntry = customtkinter.CTkEntry(master=self.formFrame, show="*", placeholder_text="Re-Enter Password", height=40, width=225)
-        self.passwordEntry.place(relx=0.75, rely=0.675, anchor=tkinter.CENTER)
+        self.phoneEntry = customtkinter.CTkEntry(master=self.formFrame, placeholder_text="Phone Number", height=40, width=125)
+        self.phoneEntry.place(relx=0.6, rely=0.5, anchor=tkinter.CENTER)
 
-        self.phoneEntry = customtkinter.CTkEntry(master=self.formFrame, placeholder_text="Phone Number", height=40, width=225)
-        self.phoneEntry.place(relx=0.75, rely=0.5, anchor=tkinter.CENTER)
+        self.adminCheck = customtkinter.CTkCheckBox(master=self.formFrame, text="Register as\nAdmin", height=40, width=100, command=self.adminCheckToggled)
+        self.adminCheck.place(relx=0.775, rely=0.5, anchor=tkinter.W)
+
+        self.passwordEntry = customtkinter.CTkEntry(master=self.formFrame, show="*", placeholder_text="Enter Password", height=40, width=225)
+        self.passwordEntry.place(relx=0.25, rely=0.675, anchor=tkinter.CENTER)
+        
+        self.passwordReEntry = customtkinter.CTkEntry(master=self.formFrame, show="*", placeholder_text="Re-Enter Password", height=40, width=225)
+        self.passwordReEntry.place(relx=0.75, rely=0.675, anchor=tkinter.CENTER)
 
         self.registerButton = customtkinter.CTkButton(master=self.formFrame, text="Register", command=self.registerFunctionFunction, height=40, width=225)
         self.registerButton.place(relx=0.75, rely=0.9, anchor=tkinter.CENTER)
 
         self.backButton = customtkinter.CTkButton(master=self.formFrame, text="Go Back", command=self.backFunction, height=40, width=80)
         self.backButton.place(relx=0.1, rely=0.9, anchor=tkinter.CENTER)
+
+    def adminCheckToggled(self):
+        if self.adminCheck.get():
+            self.phoneEntry.configure(state="disabled")
+            self.nameEntry.configure(state="disabled")
+        else:
+            self.phoneEntry.configure(state="normal", placeholder_text="Phone Number")
+            self.nameEntry.configure(state="normal", placeholder_text="Name")
 
     def registerFunctionFunction(self):
         print("Button Pressed admin window")
