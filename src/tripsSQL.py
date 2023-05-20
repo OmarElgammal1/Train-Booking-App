62a6de43a75e1c85c4de0a94a0998276cf1f7283
OmarElgammal1 <omarelgammal2004@gmail.com>
2023-05-20 18:29:35 +0300

    Modified functions for GUI usage

--- src/tripsSQL.py
+++ src/tripsSQL.py
@@ -1,23 +1,8 @@
 # Import necessary libraries
 import sys
 import pyodbc as odbc
-import pandas as pnda
 
-SERVER_NAME = 'YOUR_SERVER_NAME'
-DATABASE_NAME = 'TrainBooking'
-
-connection_string = f"""
-    DRIVER={"SQL SERVER"};
-    SERVER={SERVER_NAME};
-    DATABASE={DATABASE_NAME};
-    Trust_Connection=yes;
-"""
-
-def bookTrip(customerID, tripID, nSeats):
-    # Connect to the database
-    conn = odbc.connect(connection_string)
-    cursor = conn.cursor()
-
+def bookTrip(cursor, customerID, tripID, nSeats):
     # Check if the customer is already booked on the trip
     cursor.execute(f"""
         UPDATE SEAT
@@ -25,35 +10,18 @@ def bookTrip(customerID, tripID, nSeats):
         WHERE (tripID = '{tripID}' AND customerID IS NULL)
         LIMIT {nSeats};
     """)
-    
-    # Close the connection
-    if not conn.closed:
-        conn.close()
     return True;
 
-def cancelTrip(customerID, tripID):
-    # Connect to the database
-    conn = odbc.connect(connection_string)
-    cursor = conn.cursor()
-
+def cancelTrip(cursor, customerID, tripID):
     # Check if the customer is already booked on the trip
     cursor.execute(f"""
         UPDATE SEAT
         SET customerID = NULL
         WHERE (tripID = '{tripID}' AND customerID = '{customerID}');
     """)
-    
-    # Close the connection
-    if not conn.closed:
-        conn.close()
     return True;
 
-
-def viewCustomerTrips(customerID):
-
-    #["Trip", "Train", "From", "To", "Date", "Time", "Seats", "Price"]
-    conn = odbc.connect(connection_string)
-    cursor = conn.cursor()
+def viewCustomerTrips(cursor, customerID):
     cursor.execute(f"""
         SELECT SEAT.tripID, TRIP.trainID, TRIP.fromLocation, TRIP.toLocation, TRIP.depTime, COUNT(SEAT.seatNum) AS TotalSeats, TRIP.price
         FROM TRIP
@@ -64,11 +32,7 @@ def viewCustomerTrips(customerID):
 
     rows = cursor.fetchall()
 
-    # Close the cursor and the connection
-    cursor.close()
-    conn.close()
-
-    # ! split DATETIME and add it correctly to list
+    # split DATETIME and add it correctly to list
     # Convert the rows to a Python list
     result_list = []
     for row in rows:
@@ -81,5 +45,4 @@ def viewCustomerTrips(customerID):
             else:
                 res.append(el[0])
         result_list.append(res)
-
     return result_list;
