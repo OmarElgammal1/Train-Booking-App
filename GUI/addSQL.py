3d31e73b002ed55cd2e85ea5bd13670dfc9a097a
Yousef AlZayat <67283820+YousefAlzayat@users.noreply.github.com>
2023-05-20 19:29:28 +0300

    Update addSQL.py

--- GUI/addSQL.py
+++ GUI/addSQL.py
@@ -7,50 +7,46 @@ def addTrain(cursor, trainData):
 
 # Add a new trip to the database (done by admins only)
 # Trip data must be passed in this specifc order [trainID, fromLocation, toLocation, depTime, price]
-# def addTrip(tripData):
-#     # Check if the train exists or not before adding trip
-#     cursor.execute(f""" SELECT trainID FROM TRAIN WHERE trainID = {tripData[0]}; """)
-#     queryResult = cursor.fetchone()
-#     if queryResult is None:
-#         print("Train does NOT exist! Cannot add trip!")
-#         return ""
-#     # Test the insert query to add a new trip with the passed data
-#     try:
-#         cursor.execute(f"""
-#             INSERT INTO TRIP
-#             VALUES ({tripData[0]}, '{tripData[1]}', '{tripData[2]}', '{tripData[3]}', {tripData[4]});
-#         """)
-#     except Exception as e:
-#         cursor.rollback()
-#         print(e)
-#         print("Transaction Rollback! Insertion Failed!")
-#         return ""
-#     else:
-#         print("Trip Added Successfully!")
-#         cursor.commit()
-#     # Select query to get the trip ID
-#     selectQuery = f"""
-#         SELECT tripID FROM TRIP
-#         WHERE trainID = {tripData[0]}
-#         AND fromLocation = '{tripData[1]}'
-#         AND toLocation = '{tripData[2]}'
-#         AND depTime = '{tripData[3]}'
-#         AND price = {tripData[4]};
-#     """
-#     # Parse the DataFrame into a list
-#     listOfIDs = pnda.DataFrame(cursor.execute(selectQuery)).values.tolist()
-#     # Check for the returned number of ids, if they're more than one id
-#     # Return the last tripID that was added, otherwise return the first one
-#     if(len(listOfIDs) > 1):
-#         tripID = listOfIDs[-1][-1][0]
-#     else:
-#         tripID = listOfIDs[0][0][0]
-#     # Insert a new record in the SEAT table with customerID set to NULL
-#     cursor.execute(f""" INSERT INTO SEAT VALUES ({tripID}, NULL); """)
-#     cursor.commit()
-#     # Return the trip id that was just added
-#     print("Added trip ID:", tripID)
-#     return tripID
+def addTrip(tripData):
+    cursor.execute(f"""
+        INSERT INTO TRIP (trainID, fromLocation, toLocation, depTime, price)
+        SELECT {tripData[0]}, '{tripData[1]}', '{tripData[2]}', '{tripData[3]}', {tripData[4]}
+        WHERE NOT EXISTS (
+            SELECT * FROM TRIP
+            WHERE trainID = {tripData[0]}
+            AND depTime = '{tripData[3]}'
+        );
+    """)
+        
+    cursor.commit()
+    
+    # Select query to get the trip ID
+    selectQuery = f"""
+        SELECT tripID FROM TRIP
+        WHERE trainID = {tripData[0]}
+        AND fromLocation = '{tripData[1]}'
+        AND toLocation = '{tripData[2]}'
+        AND depTime = '{tripData[3]}'
+        AND price = {tripData[4]};
+    """
+    # Parse the DataFrame into a list
+    listOfIDs = pnda.DataFrame(cursor.execute(selectQuery)).values.tolist()
+    # Check for the returned number of ids, if they're more than one id
+    # Return the last tripID that was added, otherwise return the first one
+    if(len(listOfIDs) > 1):
+        tripID = listOfIDs[-1][-1][0]
+    else:
+        tripID = listOfIDs[0][0][0]
+    # Get the number of seats of the selected train
+    trainSeatCount = cursor.execute(f"""
+        SELECT seatCount FROM TRAIN
+        WHERE trainID = {tripData[0]}; """).fetchone()[0]
+    # Insert new records in the SEAT table with customerID set to NULL
+    # According to the number of seats in the train associated with the added trip
+    while trainSeatCount >= 1:
+        cursor.execute(f" INSERT INTO SEAT VALUES ({tripID}, NULL); ")
+        trainSeatCount -= 1
+    cursor.commit()
 
 # # Testing the addition of a new train
 # # test = [500, 'Express']
