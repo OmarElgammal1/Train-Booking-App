a0b10b74c5679c082c70e5d6a578dd3e94e0d3fc
Yousef AlZayat <67283820+YousefAlzayat@users.noreply.github.com>
2023-05-20 19:30:57 +0300

    Updated delete functions

--- src/deleteSQL.py
+++ src/deleteSQL.py
@@ -1,47 +1,12 @@
-# Import necessary libraries
-import pyodbc as odbc
-
-# Define the needed attributes for the connection string
-# Run ```SELECT @@SERVERNAME``` on MS SQL server to find YOUR_SERVER_NAME
-SERVER_NAME = 'DESKTOP-UF4LPT6'
-DATABASE_NAME = 'TrainBooking'
-
-connection_string = f"""
-    DRIVER={"SQL SERVER"};
-    SERVER={SERVER_NAME};
-    DATABASE={DATABASE_NAME};
-    Trust_Connection=yes;
-"""
-# Connect to the database
-conn = odbc.connect(connection_string)
-cursor = conn.cursor()
-
-def deleteTrip(tripID):
-
-    # Check if the customer is already booked on the trip
-    cursor.execute(f"""
-        SELECT tripID From SEAT
-        Where tripID = {tripID} AND customerID IS NOT NULL
-    """)
-
-    # Check if the customer is already booked on the trip
-    if cursor.fetchone() is not None:
-        return False;
-
-    # If no seats reserved for this trip then it gets deleted
+def deleteTrip(cursor, tripID):
     cursor.execute(f"""
         DELETE FROM SEAT
-        WHERE tripID = {tripID};
+        WHERE tripID = {int(tripID)};
         DELETE FROM TRIPS
-        WHERE tripID = {tripID};
+        WHERE tripID = {int(tripID)};
     """)
-    cursor.commit()
-    # Close the connection
-    if not conn.closed:
-        conn.close()
-    return True
 
-def deleteTrain(trainID):
+def deleteTrain(cursor, trainID):
     # Check if Train has no pending trips
     cursor.execute(f"""
         SELECT tripID From TRIP
