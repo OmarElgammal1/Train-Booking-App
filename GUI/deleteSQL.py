d177c6e00857c2e6bb7feb65fb02b1d657951350
iSeFz <seifyahia@gmail.com>
2023-05-22 11:08:19 +0300

    Moved deleteSQL to GUI/ & deleted src/

---
+++ GUI/deleteSQL.py
@@ -0,0 +1,31 @@
+# Delete certain trip by ID
+def deleteTrip(cursor, tripID):
+    cursor.execute(f"""
+        DELETE FROM SEAT
+        WHERE tripID = {tripID};
+        DELETE FROM TRIP
+        WHERE tripID = {tripID}; """)
+    cursor.commit()
+
+# Delete certain train by ID
+def deleteTrain(cursor, trainID):
+    # Check if Train has no pending trips
+    cursor.execute(f"""
+        SELECT tripID FROM TRIP
+        WHERE trainID = {trainID}
+        AND depTime > GETDATE(); """)
+
+    # Check if the customer is already booked on the trip
+    if cursor.fetchone() is not None:
+        return False
+
+    # If no trips for this train it gets deleted
+    cursor.execute(f" DELETE FROM TRAIN WHERE trainID = {trainID}; ")
+    cursor.commit()
+    return True
+
+# Delete certain user by email
+def deleteUser(cursor, email):
+    cursor.execute(f" DELETE FROM [USER] WHERE [USER].email = '{email}' ")
+    cursor.commit()
+    return True
