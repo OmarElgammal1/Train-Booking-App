fcd365c1af7cc0b662cab9713c176471c0ae46d6
iSeFz <seifyahia@gmail.com>
2023-05-25 12:04:00 +0300

    Modified database filling script

--- SQL/fillDataBase.sql
+++ SQL/fillDataBase.sql
@@ -1,186 +1,92 @@
-use TrainBooking;
--- empty tables data
-delete [USER];
-delete [ADMIN];
-delete [CUSTOMER];
-delete [TRAIN];
-delete [SEAT];
-delete [TRIP];
--- reset identity keys
-DBCC CHECKIDENT ('Train', RESEED, 0);
-DBCC CHECKIDENT ('Admin', RESEED, 0);
-DBCC CHECKIDENT ('Customer', RESEED, 0);
-DBCC CHECKIDENT ('Seat', RESEED, 0);
-DBCC CHECKIDENT ('Trip', RESEED, 0);
+USE TrainBooking
+GO
+-- Delete data from tables (optional if tables are already empty)
+DELETE FROM [USER];
+DELETE FROM [ADMIN];
+DELETE FROM [CUSTOMER];
+DELETE FROM [SEAT];
+DELETE FROM [TRIP];
+DELETE FROM [TRAIN];
+GO
+-- Reset identity columns to maintain the starting numbers of each column
+DBCC CHECKIDENT ('[ADMIN]', RESEED, 0);
+DBCC CHECKIDENT ('[CUSTOMER]', RESEED, 0);
+DBCC CHECKIDENT ('[SEAT]', RESEED, 0);
+DBCC CHECKIDENT ('[TRAIN]', RESEED, 0);
+DBCC CHECKIDENT ('[TRIP]', RESEED, 0);
+GO
+-- Insert new users into the USER table
+INSERT INTO [USER]
+VALUES
+    ('0000', 'seif@admin'),
+    ('0000', 'amgad@admin'),
+    ('0000', 'omar@admin'),
+    ('0000', 'mohsen@admin'),
+    ('0000', 'zayat@admin'),
+    ('0000', 'fatma@admin'),
+    ('0000', 'nour@admin'),
+    ('0000', 'hoda@admin'),
+    ('0000', 'ali@admin'),
+    ('0000', 'john@admin'),
+    ('1111', 'seif@user'),
+    ('1111', 'amgad@user'),
+    ('1111', 'omar@user'),
+    ('1111', 'mohsen@user'),
+    ('1111', 'zayat@user'),
+    ('1111', 'fatma@user'),
+    ('1111', 'nour@user'),
+    ('1111', 'hoda@user'),
+    ('1111', 'ali@user'),
+    ('1111', 'john@user');
+GO
+-- Insert admins into the ADMIN table
+INSERT INTO ADMIN
+SELECT email FROM [USER]
+WHERE email LIKE '%@admin%';
+GO
+-- Insert customers into the CUSTOMER table
+INSERT INTO CUSTOMER
+SELECT
+	CONCAT(UPPER(LEFT(U.email, 1)), LOWER(SUBSTRING(U.email, 2, CHARINDEX('@', U.email) - 2))) AS name,
+    '01234567890', -- Use a sample phone number
+    U.email
+FROM [USER] U
+WHERE U.email LIKE '%@user%';
+GO
+-- Insert new trains into the TRAIN table
+INSERT INTO TRAIN
+VALUES
+    (100, 'Bullet'),
+    (200, 'Express'),
+    (150, 'Local'),
+    (120, 'Commuter'),
+    (200, 'Electric'),
+    (250, 'Freight'),
+    (150, 'Maglev'),
+    (100, 'Monorail'),
+	(300, 'Regional'),
+	(80, 'Tram');
+GO
+-- Insert new trips into the TRIP table
+INSERT INTO TRIP (trainID, fromLocation, toLocation, depTime, price)
+SELECT
+    T.trainID,
+    F.fromLocation,
+    F.toLocation,
+    DATEADD(DAY, ROW_NUMBER() OVER (PARTITION BY T.trainID ORDER BY NEWID()), DATEADD(MONTH, 2, GETDATE())) AS depTime,
+    CAST((RAND(CHECKSUM(NEWID())) * 100 + 50) AS INT) AS price
+FROM
+    (SELECT DISTINCT trainID FROM TRAIN) AS T
+    CROSS JOIN (VALUES ('Cairo', 'Alex'), ('Tanta', 'Sharm El Sheikh'), ('Aswan', 'Luxor'), ('Assiut', 'Matrouh'), ('Qena', 'Mansoura')) AS F(fromLocation, toLocation);
+GO
 
--- add admins
-insert into [USER](email,password) values ('amgad@admin.com','1234')
-insert into [USER](email,password) values ('seif@admin.com','1234')
-insert into [USER](email,password) values ('zayat@admin.com','1234')
-insert into [USER](email,password) values ('omar@admin.com','1234')
+-- Add seats to trips and assign seats to customers manually from the application
 
-insert into [ADMIN] (email)
-select [USER].email from [USER]
-where [USER].email = 'amgad@admin.com';
-
-insert into [ADMIN] (email)
-select [USER].email from [USER]
-where [USER].email = 'seif@admin.com';
-
-insert into [ADMIN] (email)
-select [USER].email from [USER]
-where [USER].email = 'zayat@admin.com';
-
-insert into [ADMIN] (email)
-select [USER].email from [USER]
-where [USER].email = 'omar@admin.com';
-
--- add Customers
-insert into [USER](email,password) values ('mahmoud@gmail.com','1234')
-insert into [USER](email,password) values ('fatma@gmail.com','1234')
-insert into [USER](email,password) values ('samer@gmail.com','1234')
-insert into [USER](email,password) values ('salah@gmail.com','1234')
-
-
-insert into [CUSTOMER] (name,email,phoneNum)
-select 'Mahmoud',[USER].email,'01206212820' from [USER]
-where [USER].email = 'mahmoud@gmail.com';
-
-
-insert into [CUSTOMER] (name,email,phoneNum)
-select 'Fatma',[USER].email,'01206212820' from [USER]
-where [USER].email = 'fatma@gmail.com';
-
-insert into [CUSTOMER] (name,email,phoneNum)
-select 'Samer',[USER].email,'01206212820' from [USER]
-where [USER].email = 'samer@gmail.com';
-
-insert into [CUSTOMER] (name,email,phoneNum)
-select 'Salah',[USER].email,'01206212820' from [USER]
-where [USER].email = 'salah@gmail.com';
-
---add Trains
-insert into TRAIN values (60,'Express');
-insert into TRAIN values (30,'Local');
-insert into TRAIN values (80,'Freight');
-insert into TRAIN values (100,'High-Speed');
-
-
-
---add Trips
-insert into TRIP (trainID,fromLocation,toLocation,depTime,price)
-select [TRAIN].trainID, 'Alexandria','SharmElsheikh','2023-04-15 20:00:00',250 from TRAIN
-where [TRAIN].trainID = 1;
-
-insert into TRIP (trainID,fromLocation,toLocation,depTime,price)
-select [TRAIN].trainID, 'Cairo','SharmElsheikh','2023-04-15 20:00:00',250 from TRAIN
-where [TRAIN].trainID = 2;
-
-insert into TRIP (trainID,fromLocation,toLocation,depTime,price)
-select [TRAIN].trainID, 'Cairo','Alexandria','2023-05-16 20:00:00',250 from TRAIN
-where [TRAIN].trainID = 3;
-
-insert into TRIP (trainID,fromLocation,toLocation,depTime,price)
-select [TRAIN].trainID, 'Alexandria','SharmElsheikh','2023-06-11 15:00:00',150 from TRAIN
-where [TRAIN].trainID = 4;
-
-insert into TRIP (trainID,fromLocation,toLocation,depTime,price)
-select [TRAIN].trainID, 'Alexandria','Aswan','2023-07-21 20:00:00',500 from TRAIN
-where [TRAIN].trainID = 1;
-
-
--- add Seats
-Declare @i int = 0
-declare @seatCount int;
-
-select @seatCount = TRAIN.seatCount from TRAIN
-where [TRAIN].trainID = 1
-while @i < @seatCount
-begin
-set @i = @i + 1
-insert into SEAT (Seat.tripID)
-select TRIP.tripID from TRIP
-where TRIP.tripID = 1;
-end
-
-set @i = 0
-select @seatCount = TRAIN.seatCount from TRAIN
-where [TRAIN].trainID = 2
-while @i < @seatCount
-begin
-set @i = @i + 1
-insert into SEAT (Seat.tripID)
-select TRIP.tripID from TRIP
-where TRIP.tripID = 2;
-end
-
-set @i = 0
-select @seatCount = TRAIN.seatCount from TRAIN
-where [TRAIN].trainID = 3
-while @i < @seatCount
-begin
-set @i = @i + 1
-insert into SEAT (Seat.tripID)
-select TRIP.tripID from TRIP
-where TRIP.tripID = 3;
-end
-
-set @i = 0
-select @seatCount = TRAIN.seatCount from TRAIN
-where [TRAIN].trainID = 4
-while @i < @seatCount
-begin
-set @i = @i + 1
-insert into SEAT (Seat.tripID)
-select TRIP.tripID from TRIP
-where TRIP.tripID = 4;
-end
-
---add customer to seats
-update SEAT
-set customerID = 1
-where seatNum = 1;
-
-update SEAT
-set customerID = 1
-where seatNum = 2;
-
-update SEAT
-set customerID = 1
-where seatNum = 3;
-
-update SEAT
-set customerID = 2
-where seatNum = 61;
-
-update SEAT
-set customerID = 2
-where seatNum = 62;
-
-update SEAT
-set customerID = 2
-where seatNum = 63;
-
-update SEAT
-set customerID = 4
-where seatNum = 95;
-
-update SEAT
-set customerID = 4
-where seatNum = 104;
-
-update SEAT
-set customerID = 4
-where seatNum = 120;
-
-update SEAT
-set customerID = 4
-where seatNum = 121;
-
-update SEAT
-set customerID = 3
-where seatNum = 183;
-
-update SEAT
-set customerID = 3
-where seatNum = 184;
+-- Output the inserted data (optional)
+SELECT * FROM [ADMIN];
+SELECT * FROM [CUSTOMER];
+SELECT * FROM [SEAT];
+SELECT * FROM [TRAIN];
+SELECT * FROM [TRIP];
+SELECT * FROM [USER];
+GO
